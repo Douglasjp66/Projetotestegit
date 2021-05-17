3f40eb027e59f2ec97dca58d57ab9cb5ad42d800
Douglasjp66 <douglasrjj@gmail.com>
2021-05-17 10:41:20 -0300

    novo arquivo teste

--- testegit.py
+++ testegit.py
@@ -1,2 +1,6 @@
-print('Hello Github !')
-print('Get ready for the future')
+print('I love Python !!')
+print('Bora brincar ?')
+num1 = int(input('Numero 1:'))
+num2 =int(input('Numero 2:'))
+soma= num1 + num2
+print(f"O resultado é : {soma}")
