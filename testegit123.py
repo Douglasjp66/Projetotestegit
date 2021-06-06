c6caedabcb05305b0aef5f89011c80e6fe268723
Douglasjp66 <douglasrjj@gmail.com>
2021-06-06 09:52:48 -0300

    novo arquivo teste atualizado

--- testegit123.py
+++ testegit123.py
@@ -1,6 +1,6 @@
 print('I love Python !!')
 print('Bora brincar ?')
-num1 = int(input('Numero 1:'))
-num2 =int(input('Numero 2:'))
+num1 = int(input('Numero A:'))
+num2 =int(input('Numero B:'))
 soma= num1 + num2
 print(f"O resultado é : {soma}")
